5e6da8e0d5acf4d14e2f6c0d37765de160489056
brice <brice_morel@hotmail.com>
2019-03-17 10:35:51 +0100

    mm

--- src/app/models/croisement.ts
+++ src/app/models/croisement.ts
@@ -1,10 +1,10 @@
 import { Creneau } from "./creneau";
 import { Stand } from "./stand";
+import { Benevole } from "./benevole";
 
 export class Croisement {
   id:number;
   creneau: Creneau;
   stand: Stand;
-  description: string;
-  image: string | ArrayBuffer;
+  benevoles: Benevole[];
 }
