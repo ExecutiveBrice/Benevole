5284e0ad57d4d3cc415f70a30fe68f6ab9262048
brice <brice_morel@hotmail.com>
2019-03-21 22:27:35 +0100

    mm

--- server/controllers/croisement.js
+++ server/controllers/croisement.js
@@ -10,11 +10,13 @@ module.exports = {
   getAll: function getAll(req, res) {
     console.log("getAll")
     return Croisement.findAll({
-
+      where: {
+        'Stand.nom': 'frites'
+     },
       include: [
-        { model: Creneau },
-        { model: Stand },
-        { model: Benevole }
+        { model: Creneau, as: Creneau.tableName},
+        { model: Stand, as: Stand.tableName },
+        { model: Benevole, as: Benevole.tableName }
       ]
     })
       .then(function (croisements) {
@@ -40,7 +42,67 @@ module.exports = {
       });
   },
 
+  getByStand: function getByStand(req, res) {
+    console.log("getByStand")
+    return Croisement.findAll({
+      where: { 'Stand.id': (req.params.id)},
+      include: [
+        { model: Creneau, as: Creneau.tableName},
+        { model: Stand, as: Stand.tableName },
+      ]
+    })
+      .then(function (croisements) {
+        console.log("getByStand - 2")
+        console.log(croisements)
+        if (!croisements) {
+          return res.status(404).json({
+            title: "No croisements found for this stand",
+            error: "Please try again."
+          });
+        }
 
+        return res.status(200).json({
+          message: 'croisements found for this stand',
+          croisements: croisements
+        });
+      }).catch(function (error) {
+        console.log(error.toString());
+        return res.status(400).json({
+          title: 'There was an error signing in!',
+          error: error.stack
+        });
+      });
+  },
 
+  getByCreneau: function getByCreneau(req, res) {
+    console.log("getByCreneau")
+    return Croisement.findAll({
+      where: { 'Creneau.id': (req.params.id)},
+      include: [
+        { model: Creneau, as: Creneau.tableName},
+        { model: Stand, as: Stand.tableName },
+      ]
+    })
+      .then(function (croisements) {
+        console.log("getByCreneau - 2")
+        console.log(croisements)
+        if (!croisements) {
+          return res.status(404).json({
+            title: "No croisements found for this Creneau",
+            error: "Please try again."
+          });
+        }
 
+        return res.status(200).json({
+          message: 'croisements found for this Creneau',
+          croisements: croisements
+        });
+      }).catch(function (error) {
+        console.log(error.toString());
+        return res.status(400).json({
+          title: 'There was an error signing in!',
+          error: error.stack
+        });
+      });
+  },
 };
