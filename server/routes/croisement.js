491766656409952f03f24a351b342b2eb963f6bd
brice <brice_morel@hotmail.com>
2019-03-21 22:43:17 +0100

    mm

--- server/routes/croisement.js
+++ server/routes/croisement.js
@@ -7,6 +7,12 @@ router.get('/getAll', function(req,res,next) {
   console.log("------croisement getAll")
   return croisementController.getAll(req,res);
 });
-
-
+router.get('/getByStand', function(req,res,next) {
+  console.log("------croisement getByStand")
+  return croisementController.getByStand(req,res);
+});
+router.get('/getByCreneau', function(req,res,next) {
+  console.log("------croisement getByCreneau")
+  return croisementController.getByCreneau(req,res);
+});
 module.exports = router;
