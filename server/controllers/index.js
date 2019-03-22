2972e1e8255916d4939564708d6098d4954ef7a0
brice <brice_morel@hotmail.com>
2019-03-22 22:45:16 +0100

    ll

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,10 +3,12 @@ var produits = require('./produit');
 var users = require('./user');
 var reservations = require('./reservation');
 var croisements = require('./croisement');
+var benevoles = require('./benevole');
 var emails = require('./email');
 module.exports = {
   users: users,
   produits:produits,
+  benevoles:benevoles,
   reservations:reservations,
   croisements:croisements,
   emails:emails
