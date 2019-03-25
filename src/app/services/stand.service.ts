1423485ca8edd98098de2a262ac131116d82ca2f
brice <brice_morel@hotmail.com>
2019-03-25 22:09:26 +0100

    mm

---
+++ src/app/services/stand.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient,HttpParams} from "@angular/common/http";
+import { Stand } from '../models';
+
+@Injectable()
+export class CroisementService {
+  apiUrl = environment.apiUrl + '/stand';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  getAll() {
+    return this.http.get<Stand[]>(this.apiUrl + '/getAll', {responseType: 'json'});
+  }
+  
+
+}
