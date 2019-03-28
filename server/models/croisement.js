75f073d79290390c5064efe498a2ef2827a9ad1d
brice <brice_morel@hotmail.com>
2019-03-29 00:12:09 +0100

    mm

--- server/models/croisement.js
+++ server/models/croisement.js
@@ -31,7 +31,7 @@ module.exports = function (sequelize, DataTypes) {
         model: models.ResaBenevoles,
         unique: false
       },
-      foreignKey: 'benevole'
+      foreignKey: 'croisement'
   });
   };
   return Croisement;
