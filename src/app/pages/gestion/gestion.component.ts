cb77d6772993acf44f0771232428b0e3b05c4ff2
brice <brice_morel@hotmail.com>
2019-04-03 23:41:11 +0200

    mm

--- src/app/pages/gestion/gestion.component.ts
+++ src/app/pages/gestion/gestion.component.ts
@@ -4,6 +4,7 @@ import { BenevoleService } from '../../services';
 import { CroisementService, StandService, MailService, ConfigService } from '../../services';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Benevole, Croisement, Stand, Email } from '../../models';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-gestion',
@@ -14,7 +15,9 @@ import { Benevole, Croisement, Stand, Email } from '../../models';
 export class GestionComponent implements OnChanges {
   rappel: boolean;
   bloque: string;
-
+  rappel1: string;
+  rappel2: string;
+  benevoles: Benevole[];
   constructor(public benevoleService: BenevoleService,
     public configService: ConfigService,
     public croisementService: CroisementService,
@@ -22,8 +25,10 @@ export class GestionComponent implements OnChanges {
     public mailService: MailService,
     public sanitizer: DomSanitizer) {
     this.rappel = false;
-
+    this.benevoles = [];
     this.updateBlocage();
+    this.getText();
+    this.find();
   }
 
   ngOnChanges() {
@@ -55,6 +60,33 @@ export class GestionComponent implements OnChanges {
 
   }
 
+  find(): void {
+    console.log("find")
+    console.log(this.benevoles)
+    this.benevoleService.getAll().subscribe(data => {
+      console.log("data")
+      console.log(data)
+      this.benevoles = data['benevoles'];
+    },
+      error => {
+        console.log('😢 Oh no!', error);
+      });
+  }
+  getText() {
+    this.configService.getParam("rappel1").subscribe(res => {
+      console.log(res['param'].value);
+      this.rappel1 = res['param'].value;
+    }, err => {
+      console.log(err);
+    });
+    this.configService.getParam("rappel2").subscribe(res => {
+      console.log(res['param'].value);
+      this.rappel2 = res['param'].value;
+    }, err => {
+      console.log(err);
+    });
+  }
+  
   envoiRappel() {
     let email: Email = {
       to: "",
@@ -62,14 +94,27 @@ export class GestionComponent implements OnChanges {
       text: ""
     }
 
+    let text = "Bonjour,"+"\n"
+ text = text + this.rappel1+"\n";
+    this.benevoles.forEach(benevole => {
+      console.log(benevole)
+      benevole.Croisements.forEach(croisement => {
+        text = text + croisement.Stand.nom + " - " + croisement.Creneau.plage + "\n"
+      })
+
+    text = text + benevole.gateaux + "\n"
+    text = text + this.rappel2;
+    email.text = text
+    email.to = benevole.email
 
 
-    this.mailService.rappel(email)
+    this.mailService.sendMail(email)
       .subscribe(res => {
         console.log("this.api.sendMail");
         console.log(res);
       }, err => {
         console.log(err);
       });
+    }
   }
 }
