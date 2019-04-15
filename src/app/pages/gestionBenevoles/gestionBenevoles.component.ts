764f11ab3b4840b2b3ef5220bad4a506f6ed5464
brice <brice_morel@hotmail.com>
2019-04-16 00:07:05 +0200

    mm

--- src/app/pages/gestionBenevoles/gestionBenevoles.component.ts
+++ src/app/pages/gestionBenevoles/gestionBenevoles.component.ts
@@ -104,7 +104,7 @@ export class GestionBenevolesComponent implements OnChanges {
 
         this.email.text =  this.email.text + benevole.reponse + "<br>"
 
-        this.email.text =  this.email.text + "Vous pourrez bien entendu retrouver cette réponse sur <a href='https://ouchedinier.herokuapp.com'>le site d'inscription</a><br>Cordialement,<br>L'équipe d'animation"
+        this.email.text =  this.email.text + "<br>Vous pourrez bien entendu retrouver cette réponse sur <a href='https://ouchedinier.herokuapp.com'>le site d'inscription</a><br>Cordialement,<br>L'équipe d'animation"
         this.envoiMail(this.email)
       },
         error => {
