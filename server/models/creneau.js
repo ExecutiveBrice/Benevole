198b6604de8ece0e72f90db8aa00f17a32146063
brice <brice_morel@hotmail.com>
2019-04-17 00:09:03 +0200

    mm

--- server/models/creneau.js
+++ server/models/creneau.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.INTEGER,
     },
     chevauchement: {
-      type: DataTypes.STRING,
+      type: DataTypes.ARRAY(DataTypes.INTEGER),
     },
   });
   Creneau.associate = function(models) {
