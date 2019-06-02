4137ef130c9a078b1edb54b2033f7c1378563d79
brice <brice_morel@hotmail.com>
2019-06-02 09:51:36 +0200

    mm

--- server/controllers/benevole.js
+++ server/controllers/benevole.js
@@ -4,10 +4,10 @@ var Croisement = require('../models').Croisement;
 var Creneau = require('../models').Creneau;
 var Stand = require('../models').Stand;
 var Benevole = require('../models').Benevole;
+let idsToSkip = []; //generate array of ids, this may result in empty array
+idsToSkip.push('-1'); // push a value that can not be the id in table. this will generate `not in ('-1')` query serving the purpose.
 
 module.exports = {
-
-
 
   getAll: function getAll(req, res) {
     console.log("getAll")
@@ -24,6 +24,88 @@ module.exports = {
     })
       .then(function (benevoles) {
         console.log("getAll - 2")
+        console.log(benevoles)
+        if (benevoles.length == 0) {
+          return res.status(404).json({
+            title: "No benevoles found",
+            error: "Please try again."
+          });
+        }
+
+        return res.status(200).json({
+          message: 'benevoles found',
+          benevoles: benevoles
+        });
+      }).catch(function (error) {
+        console.log(error.toString());
+        return res.status(400).json({
+          title: 'There was an error signing in!',
+          error: error.stack
+        });
+      });
+  },
+
+  getWithChoice: function getWithChoice(req, res) {
+    console.log("getWithChoice")
+    return Benevole.findAll({
+      include: [
+        {
+          model: Croisement,
+          where: {
+            id: {
+              $notIn: idsToSkip
+            }
+          },
+          include: [
+            { model: Creneau },
+            { model: Stand }
+          ]
+        }
+      ]
+    })
+      .then(function (benevoles) {
+        console.log("getWithChoice - 2")
+        console.log(benevoles)
+        if (benevoles.length == 0) {
+          return res.status(404).json({
+            title: "No benevoles found",
+            error: "Please try again."
+          });
+        }
+
+        return res.status(200).json({
+          message: 'benevoles found',
+          benevoles: benevoles
+        });
+      }).catch(function (error) {
+        console.log(error.toString());
+        return res.status(400).json({
+          title: 'There was an error signing in!',
+          error: error.stack
+        });
+      });
+  },
+
+  getWithOutChoice: function getWithOutChoice(req, res) {
+    console.log("getWithOutChoice")
+    return Benevole.findAll({
+      include: [
+        {
+          model: Croisement,
+          include: [
+            { model: Creneau },
+            { model: Stand }
+          ]
+        }
+      ],
+      where: sequelize.where(
+        sequelize.col('Croisement.id'),
+        'IS',
+        null
+    )
+    })
+      .then(function (benevoles) {
+        console.log("getWithOutChoice - 2")
         console.log(benevoles)
         if (benevoles.length == 0) {
           return res.status(404).json({
