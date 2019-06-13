1188b3d8ed5ec2ddfa9556010aa03613dc4605a3
brice <brice_morel@hotmail.com>
2019-06-13 23:15:08 +0200

    mm

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule} from "../app-routing.module";
 import { FormsModule} from "@angular/forms";
 import { HttpClientModule} from "@angular/common/http";
 import { OrderByPipe} from "../sort.pipe";
-import { UserService, CroisementService, BenevoleService, StandService, MailService, ConfigService} from "../services";
+import { UserService, CroisementService, BenevoleService, StandService, MailService, ConfigService, ExcelService} from "../services";
 import { LoginComponent} from "../pages/login/login.component";
 import { SignupComponent} from "../pages/signup/signup.component";
 import { DashboardComponent} from "../pages/dashboard/dashboard.component";
@@ -34,7 +34,8 @@ import { Ng2ImgMaxModule } from 'ng2-img-max';
     BenevoleService,
     StandService,
     MailService,
-    ConfigService
+    ConfigService,
+    ExcelService
   ],
   declarations: [
     LoginComponent,
