1fd6fa5b7aced79c1179b1c064c4158d7a97dac7
brice <brice_morel@hotmail.com>
2019-06-16 20:22:07 +0200

    mm

--- src/app/services/excel.service.ts
+++ src/app/services/excel.service.ts
@@ -13,21 +13,54 @@ export class ExcelService {
   public multiExportAsExcelFile(json: any[], excelFileName: string): void {
     /* generate workbook and add the worksheet */
     const workbook: XLSX.WorkBook = XLSX.utils.book_new();
-
+    var wscols = [
+      { wch: 20 },
+      { wch: 20 },
+      { wch: 20 },
+      { wch: 20 },
+      { wch: 20 },
+      { wch: 20 },
+      { wch: 20 },
+      { wch: 20 },
+      { wch: 20 },
+      { wch: 20 }
+    ];
     json.forEach(page => {
-      const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(page.creneaux);
-      console.log('worksheet',worksheet);
+      var worksheet = XLSX.utils.aoa_to_sheet([
+        [""],
+        [page.nom]
+      ]);
+
+      var wsrows = [
+        { hpx: 24, level: 3 },
+        { hpx: 24, level: 3 },
+        { hpx: 24, level: 3 },
+        // { hidden: true },
+        // { hidden: false }
+      ];
+
+
+      XLSX.utils.sheet_add_json(worksheet, page.creneaux, { origin: "A4" });
+
+      worksheet['!cols'] = wscols;
+      worksheet['!rows'] = wsrows;
+      console.log('worksheet', worksheet);
       XLSX.utils.book_append_sheet(workbook, worksheet, page.nom);
     });
+
+
+
+
+
     const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
     this.saveAsExcelFile(excelBuffer, excelFileName);
   }
 
 
   public exportAsExcelFile(json: any[], excelFileName: string): void {
-    
+
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
-    console.log('worksheet',worksheet);
+    console.log('worksheet', worksheet);
     const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
     const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
     this.saveAsExcelFile(excelBuffer, excelFileName);
