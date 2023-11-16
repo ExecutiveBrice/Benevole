17e6621111a74474c963795c21084f72ee19c825
ExecutiveBrice <brice_morel@hotmail.com>
2023-11-17 00:13:29 +0100

    Corrections docker-compose.yml

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  
-  //url: "/api_benevole/"
-  url: "https://bmoexperience.fr/api_benevole/"
-  //url: "https://alod.fr/api_benevole/"
+  url:'api/'
 };
