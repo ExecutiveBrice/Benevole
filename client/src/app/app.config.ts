0a68ea828b9aba86a1d2517605065583be04b700
Brice MOREL <brice@alnitaka.com>
2026-09-24 13:31:14 +0200

    applicationPWA

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
@@ -7,10 +7,15 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { jwtAuthenticationInterceptor } from './services/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideHttpClient(withInterceptors([jwtAuthenticationInterceptor])), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
     {provide: LocationStrategy, useClass: HashLocationStrategy},
       provideAnimations(),
+      provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+      }),
     ]
 };
